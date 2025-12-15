1c188cc7767c1f8f3ef9c1132aa2abe13aa03957
kyyyyyyyyy <rayanbensaid001@gmail.com>
2025-12-15 21:00:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
-HASHED_PASS=$(tor --hash-password abcdef| grep 16)
-sed -i "s/#HashedControlPassword 16:x/HashedControlPassword $HASHED_PASS/g" /etc/tor/torrc
 
-tor -f /etc/tor/torrc &>/dev/null &
-n=0
+echo "Starting Tor..."
+service tor start
 
-# continue until $n equals 5
-while [ $n -le $2 ]
-do
-	# shellcheck disable=SC2068
-	python3 hit.py $@
-	python3 refreship.py
-	n=$(( n+1 ))	 # increments $n
-done
+sleep 10
+
+echo "Sending traffic..."
+python3 /scripts/hit.py -u https://evanappmv.blogspot.com/2025/12/yt.html -l 3 -w 5 -d True
+
+tail -f /dev/null
